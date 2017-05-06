0e343b9172f044e76ee6ee5d8b39ef81efcbd545
andrewgurung <andrewgurungg@gmail.com>
2017-05-06 18:31:23 -0400

    Load samples with button

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,11 @@ var App = React.createClass({
     // set the state to re-render only the changed fishes state
     this.setState({ fishes: this.state.fishes });
   },
+  loadSamples: function() {
+    this.setState({
+      fishes: require('./sample-fishes')
+    });
+  },
   render: function() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +40,7 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-      <Inventory addFish={this.addFish}/>
+      <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
@@ -125,7 +130,8 @@ var Inventory = React.createClass({
     return (
       <div>
         <h2>Inventory</h2>
-        <AddFishForm {...this.props}  />
+        <AddFishForm {...this.props} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
