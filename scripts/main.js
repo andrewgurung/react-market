5a808dbecdabab6eceadad7f4917b27b00beab64
andrewgurung <andrewgurungg@gmail.com>
2017-05-07 09:18:33 -0400

    Display order total using JSX

--- scripts/main.js
+++ scripts/main.js
@@ -152,6 +152,21 @@ var Header = React.createClass({
   <Order />
 */
 var Order = React.createClass({
+  renderOrder: function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+    if(!fish) {
+      return <li key={key}>Sorry, fish no longer available</li>
+    }
+
+    return (
+      <li>
+        {count}lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
   render: function() {
     var orderIds = Object.keys(this.props.order);
 
@@ -167,14 +182,14 @@ var Order = React.createClass({
       return prevTotal;
     }, 0);
 
-
     return (
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
         <ul className="order">
+          {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
-            {total}
+            {h.formatPrice(total)}
           </li>
         </ul>
       </div>
